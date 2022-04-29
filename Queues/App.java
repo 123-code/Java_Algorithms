75fa3698c22a83a9e190fffcbb2693135ff41b1d
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2022-04-29 19:06:33 +0000

    App.java

--- Queues/App.java
+++ Queues/App.java
@@ -1,9 +1,31 @@
 package Queues;
 import Queues.ArrayList.Queue;
+import java.util.Scanner;
 public class App {
     
     public void main(String[] args) throws Exception {
+      Scanner scan = new Scanner(System.in);
         Queue cola = new Queue();
-        System.out.println("Hello World!");
+      int op;
+      do{
+        System.out.println("1 Ingresar Nombre 2. Eliminar persona 3. Salir");
+        op = Integer.parseInt(scan.nextLine());
+switch(op){
+
+      Case 1:
+        String nombre = scan.nextLine();
+        Persona persona = new Persona();
+        persona.setNombre(nombre);
+
+        int edad = Integer.parseInt(scan.nextLine());
+        persona.setEdad(edad);
+        break;
+        
+        Case 2:
+        
+}
+            
+      }while(op!=3);
+        
     }
 }
--- Queues/Persona.java
+++ Queues/Persona.java
@@ -1,5 +1,21 @@
 package Queues;
+import Queues.ArrayList.Queue;
 
 public class Persona {
+  private String Nombre;
+  private int edad;
+
+  public Persona(){
+    
+  }
+
+public void setNombre(String _Nombre){
+  this.Nombre = _Nombre;
+}
+
+public String getNombre(){
+  return this.Nombre;
+}
+
     
 }
