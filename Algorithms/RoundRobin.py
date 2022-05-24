9268475be153ff8f552a392dcd52d2df3296506e
123-code <naranjojose256@gmail.com>
2022-05-23 22:37:01 -0500

    Python RoundRobin

---
+++ Algorithms/RoundRobin.py
@@ -0,0 +1,24 @@
+from queue import Queue
+
+q = Queue(maxsize=100)
+print("Round Robin Algorithm python")
+print(q.qsize())
+
+class queue:
+    
+    def __init__(self,data):
+        self.data = data
+    
+    def add(element):
+        q.put(element)
+    def remove():
+        q.get(0)
+
+    def peek():
+        return q[0]
+    def getqueue():
+        print(q)
+
+
+def RoundRobin(qtm,time):
+    pass
