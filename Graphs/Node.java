92fe6953cf7199bc08bcc323558119e375a2f3b2
123-code <naranjojose256@gmail.com>
2022-06-17 23:20:06 -0500

    Graph created In Java

---
+++ Graphs/Node.java
@@ -0,0 +1,20 @@
+package Graphs;
+
+public class Node
+{
+    private String Name;
+
+public Node(){
+    
+
+}
+
+public String getName(){
+    return Name; 
+}
+
+public void SetName(String _Name){
+this.Name = _Name;
+}
+
+}
