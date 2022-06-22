7e3ee0f59efd4f933034f287711b5dcbd6cd2bd3
123-code <naranjojose256@gmail.com>
2022-06-21 23:15:23 -0500

    Started Binary tree implementation

---
+++ Binary Trees/Node.java
@@ -0,0 +1,13 @@
+package Binary Trees;
+
+public class Node {
+    
+        private  Empleado dato;
+        private Node izquierdo;
+        private Node derecho;
+    
+      public Node(){
+          
+      }  
+    }
+    
