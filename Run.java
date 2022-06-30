f0661563b61128a1d423779c2452cf7efba3fcde
123-code <naranjojose256@gmail.com>
2022-06-29 22:44:53 -0500

    Added Test Code

--- Run.java
+++ Run.java
@@ -9,7 +9,7 @@ Scanner scan = new Scanner(System.in);
 
         int op;
         do{
-            System.out.println("1. Crear grafo 2. Agregar nodos 3. Agregar conexiones 4. Salir");
+            System.out.println("1. Crear grafo 2. Agregar nodos 3. Agregar conexiones 4. Comprobar Conexión 5. Salir");
             op = Integer.parseInt(scan.nextLine());
             switch(op){
                 case 1:
@@ -31,9 +31,9 @@ Scanner scan = new Scanner(System.in);
                     break;
 
                 case 3:
-                System.out.println("Nodo 1 a agregra conexion");
+                System.out.println("Nodo 1 a agregr a conexion");
                 int _vertices = Integer.parseInt(scan.nextLine());
-                System.out.println("Nodo 2 a agregra conexion");
+                System.out.println("Nodo 2 a agregr a conexion");
                 int _vertices1 = Integer.parseInt(scan.nextLine());
 
                 if(graph.checkconnection(_vertices,_vertices1)==true){
@@ -44,13 +44,31 @@ Scanner scan = new Scanner(System.in);
             }
                     break;
 
+            case 4:
+            System.out.println("COMPROBACION DE CONEXIONES");
+            System.out.println("Nodo 1 a comprobar la conexion");
+            int vertices1 = Integer.parseInt(scan.nextLine());
+            System.out.println("Nodo 2 a comprobar la conexion");
+            int vertices2 = Integer.parseInt(scan.nextLine());
+
+            if(graph.checkconnection(vertices1,vertices2)==true){
+                System.out.println("Existe la conexion");
+            }
+            else if(graph.checkconnection(vertices1,vertices2)==false){
+                System.out.println("No existe la conexion");
+            }
+            
+
+            break;
+
+ 
 
                 
 
 
                 
                     
-                case 4:
+                case 5:
                     System.out.println(""+graph.toString());
                     break;
             }
